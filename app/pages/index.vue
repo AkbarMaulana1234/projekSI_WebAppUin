a97cb8d667dcb1466acf202cfd43a61971aeb2e3
akbar maulana <123akbarm4ul4n4@gmail.com>
2026-03-29 14:17:31 +0700

    menambahkan module database drizzel orm dan beserta setupnya

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,13 @@
 <template>
   <div class="bg-color-primary text-primary">akbar</div>
+  <div v-if="data">
+    <p>{{ data }}</p>
+  </div>
 </template>
+<script setup>
+  import { onMounted } from "vue";
+  const data = await useFetch("/api/helloWorld");
+  onMounted(() => {
+    console.log("mounted");
+  });
+</script>
